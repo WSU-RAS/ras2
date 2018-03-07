e00459575666eb74297ff6ca579aa3a5bec532ca
Shivam <goelshivam1210@gmail.com>
2018-03-07 10:44:00 -0800

    added submodule in the repository

--- src/scheduler/scripts/findperson_smach.py
+++ src/scheduler/scripts/findperson_smach.py
@@ -3,19 +3,19 @@
 import rospy
 import smach
 import smach_ros
+import actionlib
 from smach_ros import SimpleActionState
 
 # roslib.load_manifest('smach_tutorials')
-roslib.load_manifest('FindPerson')
+# roslib.load_manifest('FindPerson')
 
 from actionlib import SimpleActionServer
-from ras_msgs.msg import GotoAction, GotoFeedback, GotoResult
+# from ras_msgs.msg import GotoAction, GotoFeedback, GotoResult
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from actionlib_msgs.msg import *
-import actionlib
 from actionlib_msgs.msg import GoalStatus
 
-from find_person.msg import FindPersonAction, FindPersonResult
+from find_person.msg import FindPersonAction, FindPersonResult, FindPersonGoal
 
 
 # class to find the person
@@ -26,14 +26,14 @@ from find_person.msg import FindPersonAction, FindPersonResult
 # it has two options, it either finds it or there is an error
 
 # input should  Goal: task number and error step
-# output should be Result: Found, X, Y
+# output should be Result: Found, location[X, Y]
 
 class FindPerson(smach.State):
 
 	def __init__(self):	
 		smach.State.__init__(self, outcomes = ['found', 'error'],\
 							output_keys = ['find_person_output'])
-		self.counter = 0
+		# self.counter = 0
 
 	def execute(self, userdata):
 		pass
@@ -51,11 +51,9 @@ class GotoXY(smach.State):
 		return 'done'
 		pass
 
-
-		# here is the part where we trigger Chris' part where we have the coordinates and
-		# just need to navigate the robot through the map
-
 def main():
+	# find_person_goal.task_number = 1
+	# findperson_goal.error_step = 4
 	rospy.init_node("find_person")
 	
 	# SMACH State Machine
@@ -63,10 +61,10 @@ def main():
 		input_keys = ['sm_input'], \
 		output_keys = ['sm_output'])
 	
-	# # dummy goal set for testing
-	# findperson_goal = FindPersonGoal()
-	# findperson_goal.task_number = 2
-	# findperson_goal.error_step = 3
+	# dummy goal set for testing
+	find_person_goal = FindPersonGoal()
+	find_person_goal.task_number = 2
+	find_person_goal.error_step = 3
 
 	# container having all the states
 	with sm:
@@ -77,26 +75,28 @@ def main():
 			return result 
 
 		smach.StateMachine.add('FINDPERSON', smach_ros.SimpleActionState('find_person', FindPersonAction,\
-			goal = tasknumber, error_step, \
-			result_cb = findperson_result_cb) \
-			transitions = {'found': 'GOTOXY', 'error': 'error'}, \
+			goal = find_person_goal),  \
+			# result_cb = findperson_result_cb) \
+			transitions = {'succeeded': 'GOTOXY', 'aborted': 'error', 'preempted': 'error'}, \
 			remapping = {'find_person_output' : 'goto_input'})
 		
 		def goto_goal_cb(x, y):
-	        goal = MoveBaseGoal()
-    	    goal.target_pose.header.frame_id = "map"
-        	goal.target_pose.header.stamp = rospy.Time.now()
-        	goal.target_pose.pose.position.x = x
-        	goal.target_pose.pose.position.y = y
+			goal = MoveBaseGoal()
+			goal.target_pose.header.frame_id = "map"
+			goal.target_pose.header.stamp = rospy.Time.now()
+			goal.target_pose.pose.position.x = x
+			goal.target_pose.pose.position.y = y
 			goal.target_pose.pose.orientation.w = 1.0 # Go forward
+			return 'done'
 
 
 		smach.StateMachine.add('GOTOXY', smach_ros.SimpleActionState('move_base', MoveBaseAction, goal_cb = goto_goal_cb), \
-			transitions = {'done': 'finish', 'error': 'error'}, \
+			transitions = {'succeeded': 'finish', 'aborted': 'error', 'preempted': 'error'}, \
 			remapping = {'goto_input' : 'find_person_output'})
 		
 		
-
+	sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+	sis.start()
 	outcome =  sm.execute()
 	rospy.signal_shutdown('All done.')
 
