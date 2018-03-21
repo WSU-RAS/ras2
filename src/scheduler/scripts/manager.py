202db7bf336a4fc98160e84f0cf1caf8eda4cfef
gabrieledcjr <gabrieledcjr@gmail.com>
2018-03-21 14:10:43 -0600

    clean up code

--- src/scheduler/scripts/manager.py
+++ src/scheduler/scripts/manager.py
@@ -9,8 +9,68 @@ from ras_msgs.msg import DoErrorAction, DoErrorFeedback, DoErrorResult
 from ras_msgs.msg import TabletGotoAction, TabletGotoFeedback, TabletGotoResult
 from tablet_interface.srv import Tablet
 
-class SchedulerServer:
+from adl.util import Task, Goal
 
+
+class TabletData(object):
+    audio_url = "" # TODO nobody recorded the audio files yet?
+    face_url = "happy-cartoon-face-hi.png"
+    basename = 'http://casas.wsu.edu/smarthomestats/video/'
+    
+    def get_data(self, task_number, error_step):
+        """
+        From the task number and error step, get the corresponding object name
+        and video urls
+        """
+        object_to_find = '' # No object for this error / step
+        video_step_url = ''
+        video_full_url = ''
+
+        
+        if task_number == Task.WATER_PLANTS:
+           video_full_url = self.basename + 'waterplants.all.mp4'
+           if error_step in [0, 1, 4, 5]:
+               video_step_url = self.basename + 'waterplants.error1.mp4'
+               object_to_find = 'watercan'
+           elif error_step == 2:
+               video_step_url = self.basename + 'waterplants.error2.mp4'
+               object_to_find = 'plantcoffee'
+           elif error_step == 3:
+               video_step_url = self.basename + 'waterplants.error3.mp4'
+               object_to_find = "plantside"
+
+        elif task_number == Task.TAKE_MEDS:
+            video_full_url = self.basename + 'takemedication.all.mp4'
+            if error_step in [0, 5, 11]:
+                video_step_url = self.basename + 'takemedication.error1.mp4'
+                object_to_find = 'food'
+            elif error_step in [1, 2, 7, 10]:
+                video_step_url = self.basename + 'takemedication.error2.mp4'
+                object_to_find = 'glass'
+            elif error_step in [3, 6, 9]:
+                video_step_url = self.basename + 'takemedication.error3.mp4'
+                object_to_find = 'pillbottle'
+
+        elif task_number == Task.WALK_DOG:
+            video_full_url = self.basename + 'walkdog.all.mp4'
+            if error_step == 0:
+                video_step_url = self.basename + 'walkdog.error1.mp4'
+                object_to_find = 'umbrella'
+            elif error_step == 1:
+                video_step_url = self.basename + 'walkdog.error2.mp4'
+                object_to_find = 'leash'
+            elif error_step == 2:
+                video_step_url = self.basename + 'walkdog.error3.mp4'
+                object_to_find = 'keys'
+            elif error_step == 3:
+                video_step_url = self.basename + 'walkdog.error4.mp4'
+                object_to_find = 'dog'
+
+        return object_to_find, video_step_url, video_full_url
+
+
+class SchedulerServer:
+    
     def __init__(self):
         self.is_goto_active = False
         self.success = False
@@ -43,87 +103,34 @@ class SchedulerServer:
         # Tablet data
         self.task_number = 0
         self.error_step = 0
-        self.audio_url = ""
-        self.face_url = ""
-        self.object = ""
+        self.object_name = ""
         self.video_step_url = ""
         self.video_full_url = ""
+        self.is_task_error_completed = False
+        self.is_robot_at_base = True
 
     def shutdown(self):
         if self.is_goto_active:
             self.goto_client.cancel_goal()
 
-    def error_data(self, task_number, error_step):
-        """
-        From the task number and error step, get the corresponding object name
-        and video urls
-        """
-        object_to_find = '' # No object for this error / step
-        video_step_url = ''
-        video_full_url = ''
-
-        basename = 'http://casas.wsu.edu/smarthomestats/video/'
-
-        # Water plants
-        if task_number == 0:
-           video_full_url = basename + 'waterplants.all.mp4'
-           if error_step in [0, 1, 4, 5]:
-               video_step_url = basename + 'waterplants.error1.mp4'
-               object_to_find = 'watercan'
-           elif error_step == 2:
-               video_step_url = basename + 'waterplants.error2.mp4'
-               object_to_find = 'plantcoffee'
-           elif error_step == 3:
-               video_step_url = basename + 'waterplants.error3.mp4'
-               object_to_find = "plantside"
-        # Take Meds
-        elif task_number == 1:
-            video_full_url = basename + 'takemedication.all.mp4'
-            if error_step in [0, 5, 11]:
-                video_step_url = basename + 'takemedication.error1.mp4'
-                object_to_find = 'food'
-            elif error_step in [1, 2, 7, 10]:
-                video_step_url = basename + 'takemedication.error2.mp4'
-                object_to_find = 'glass'
-            elif error_step in [3, 6, 9]:
-                video_step_url = basename + 'takemedication.error3.mp4'
-                object_to_find = 'pillbottle'
-        # Walk the dog
-        elif task_number == 2:
-            video_full_url = basename + 'walkdog.all.mp4'
-            if error_step == 0:
-                video_step_url = basename + 'walkdog.error1.mp4'
-                object_to_find = 'umbrella'
-            elif error_step == 1:
-                video_step_url = basename + 'walkdog.error2.mp4'
-                object_to_find = 'leash'
-            elif error_step == 2:
-                video_step_url = basename + 'walkdog.error3.mp4'
-                object_to_find = 'keys'
-            elif error_step == 3:
-                video_step_url = basename + 'walkdog.error4.mp4'
-                object_to_find = 'dog'
-
-        return object_to_find, video_step_url, video_full_url
-
-    def tablet_command(self, screen, object_name, face_url, video_step_url,
-            video_full_url, audio_url):
+    def setup_tablet(self, screen):
         """
         Command the tablet to switch to a particular screen
         """
         rospy.wait_for_service("tablet")
 
         rospy.loginfo("Commanding tablet: s: %s o: %s f: %s vs: %s vf: %s a: %s",
-                screen, object_name, face_url, video_step_url,
-                video_full_url, audio_url)
+                screen, self.object_name, TabletData.face_url, self.video_step_url,
+                self.video_full_url, TabletData.audio_url)
 
         try:
             query = rospy.ServiceProxy("tablet", Tablet)
-            results = query(screen, object_name, face_url, video_step_url,
-            video_full_url, audio_url)
+            results = query(
+                screen, self.object_name, TabletData.face_url, 
+                self.video_step_url, self.video_full_url, TabletData.audio_url)
             return results.success
         except rospy.ServiceException, e:
-            rospy.logerr("Service call failed: %s" % e)
+            rospy.logerr("Service call failed: {}".format(e))
 
         return False
 
@@ -133,7 +140,7 @@ class SchedulerServer:
         """
         success = True
         response = goal.response
-        rospy.loginfo("Got message from tablet: "+response)
+        rospy.loginfo("Got message from tablet: {}".format(response))
 
         if response == "audioplay":
             pass
@@ -142,11 +149,10 @@ class SchedulerServer:
         elif response == "yes":
             pass
         elif response == "no":
-            pass
+            self.is_task_error_completed = True
         elif response == "videodone":
             # Return to the options screen
-            self.tablet_command("options", self.object, self.face_url,
-                    self.video_step_url, self.video_full_url, self.audio_url)
+            self.setup_tablet("options")
         elif response == "audiodone":
             pass
         elif response == "watchfull":
@@ -155,12 +161,10 @@ class SchedulerServer:
             pass
         elif response == "goto":
             # Show options but without the go to object button
-            self.object = ""
-            self.tablet_command("options", self.object, self.face_url,
-                    self.video_step_url, self.video_full_url, self.audio_url)
+            self.setup_tablet("options")
 
             # Find the object
-            if self.goto(self.object, self.task_number, self.error_step):
+            if self.goto(Goal.OBJECT):
                 while not self.is_goto_active:
                     self.rate.sleep()
 
@@ -199,22 +203,24 @@ class SchedulerServer:
         What to do when error detection calls this action server, when an error
         is detected
         """
+        self.is_task_error_completed = False
         rospy.loginfo("Executing Do Error")
 
-        # For use on tablet
-        self.audio_url = "" # TODO nobody recorded the audio files yet?
-        self.face_url = "happy-cartoon-face-hi.png"
-        self.object, self.video_step_url, self.video_full_url = \
-                self.error_data(goal.task_number, goal.error_step)
         self.task_number = goal.task_number
         self.error_step = goal.error_step
 
-        # Command tablet
-        self.tablet_command("choice", self.object, self.face_url,
-                self.video_step_url, self.video_full_url, self.audio_url)
+        # For use on tablet
+        self.object_name, self.video_step_url, self.video_full_url = \
+            TabletData.get_data(self.task_number, self.error_step)
+
+        # Setup tablet with videos and commands for the task error step
+        self.setup_tablet("choice")
+        
+        while not self.is_task_error_completed:
+            break
 
         # Find the human (type 1)
-        if self.goto("human", goal.task_number, goal.error_step):
+        if self.goto(Goal.HUMAN):
             while not self.is_goto_active:
                 self.rate.sleep()
 
@@ -239,6 +245,7 @@ class SchedulerServer:
             do_error_result.is_complete = True
             self.do_error.set_succeeded(do_error_result)
 
+            # Success is determined in do_error done callback
             if self.success:
                 rospy.loginfo("Found human")
             else:
@@ -250,28 +257,19 @@ class SchedulerServer:
             do_error_result.is_complete = True
             self.do_error.set_succeeded(do_error_result)
 
-    def goto(self, object_name, task_number, error_step):
+    def goto(self, goto_type):
         """
         From the specified server, try going to the specified object
         """
-        if object_name == "base1":
-            goto_type = 0
-        elif object_name == "base2":
-            goto_type = 0
-        elif object_name == "human":
-            goto_type = 1
-        else:
-            goto_type = 2
-
         if self.is_goto_active:
             rospy.logerr("Cannot navigate to two places at once!")
             return False
 
         goto_goal = GotoGoal()
         goto_goal.type = goto_type
-        goto_goal.task_number = task_number
-        goto_goal.error_step = error_step
-        goto_goal.error_object = error_step
+        goto_goal.task_number = self.task_number
+        goto_goal.error_step = self.error_step
+        goto_goal.error_object = self.error_step
         self.goto_client.send_goal(
             goto_goal,
             done_cb=self.goto_done_cb,
