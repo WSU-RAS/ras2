1090c05ac15e92e6a3cd29aa81b15fc3f965d5c2
Garrett Wilson <g@floft.net>
2018-04-04 16:03:24 -0700

    systemd script now runs via teleop

--- src/scheduler/scripts/ras_run.sh
+++ src/scheduler/scripts/ras_run.sh
@@ -3,12 +3,16 @@
 source /opt/ros/kinetic/setup.bash
 source ~/cartographer_ws/install_isolated/setup.bash
 source ~/ras/devel/setup.bash
-export ROS_IP=10.42.0.1
-export ROS_MASTER_URI=http://$ROS_IP:11311
+
+# Jetson
+#export ROS_IP=10.42.0.1
+#export ROS_MASTER_URI=http://$ROS_IP:11311
+export ROS_MASTER_URI=http://localhost:11311 # for _teleop
 export TURTLEBOT_3D_SENSOR=astra
 export TURTLEBOT3_MODEL=waffle
 
 # Launch
-roslaunch scheduler ras_experiment.launch
+#roslaunch scheduler ras_experiment.launch
+roslaunch scheduler ras_experiment_teleop.launch
 
 # Use 'rviz' alias to access the rviz GUI
