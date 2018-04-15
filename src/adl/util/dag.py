c62c37ef65512b6faef61f702d5b1d2297185bdc
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2018-04-14 23:00:11 -0600

    change next dage for take_med error

--- src/adl/util/dag.py
+++ src/adl/util/dag.py
@@ -245,7 +245,7 @@ class TakeMedicationDag(object):
         3:  ('Retrieve medication', 'pillbottle', 'M' , get_med      , eat_food),
         4:  ('Sit chair'          , None        , 'CH', sit_chair    , take_med),
         5:  ('Eat food'           , None        , 'F' , eat_food     , drink_water),
-        6:  ('Take medication'    , None        , 'M' , take_med     , drink_water),
+        6:  ('Take medication'    , None        , 'M' , take_med     , stand_up),
         7:  ('Drink water'        , None        , 'C' , drink_water  , rinse_cup),
         8:  ('Stand up'           , None        , 'CH', stand_up     , throw_garbage),
         9:  ('Rinse cup'          , None        , 'C' , rinse_cup    , None),
