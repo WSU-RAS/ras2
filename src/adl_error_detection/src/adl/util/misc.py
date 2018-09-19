076a0c8b996c6ce19bcebc93e70f6334c33c0ca5
gabrieledcjr <gabrieledcjr@gmail.com>
2018-09-18 17:11:25 -0700

    add class for terminal colors for logs

--- src/adl_error_detection/src/adl/util/misc.py
+++ src/adl_error_detection/src/adl/util/misc.py
@@ -6,3 +6,13 @@ def get_mac():
     mac = getnode()
     serial = ''.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
     return serial
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
