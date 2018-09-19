9aed72b1a462718a3abca393db079fa95933260d
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2018-09-18 18:00:27 -0700

    now uses threading, use time difference for real-time sim

--- src/adl_error_detection/scripts/test_error_detector.py
+++ src/adl_error_detection/scripts/test_error_detector.py
@@ -8,10 +8,13 @@ import configparser
 import csv
 import uuid
 import time
+import sys
+import threading
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from adl.util import ConnectPythonLoggingToRos
 from adl.util import Task
+from adl.util import get_mac
 from casas import objects, rabbitmq
 
 from ras_msgs.msg import TaskStatus, TaskId
@@ -20,14 +23,17 @@ from ras_msgs.srv import TaskController, Pause, PauseResponse
 
 class TestErrorDetector:
 
-    def __init__(self):
+    def __init__(self, speed=1):
         rospy.loginfo("Initializing Test Error Detector")
 
         rospack = rospkg.RosPack()
         self.pkg_path = rospack.get_path('adl_error_detection')
 
+        self.mac_address = get_mac()
+        self.speed = speed
         self.pause = False
         self.task_number = None
+        self.thread = None
         self.task_status = TaskStatus(
             status=TaskStatus.PENDING,
             text='PENDING')
@@ -95,29 +101,31 @@ class TestErrorDetector:
             rospy.logerr("Service call failed: {}".format(e))
 
     def load_test_events(self, file_name):
-        self.test_event_index = 0
-        self.test_events_ctr = 0
-        self.test_events = defaultdict(list)
-        data_file = self.pkg_path + "/data/" + file_name
-        rospy.loginfo("Loading data from {}".format(file_name))
-        with open(data_file) as csvfile:
-            data = csv.reader(csvfile, delimiter='\t', quotechar='|')
-            self.start_datetime = None
-            for row in data:
-                rospy.loginfo(row[0][:-7])
-                if len(row) == 0:
-                    break
-                dt = datetime.datetime.strptime(row[0][:-7], "%Y-%m-%d %H:%M:%S")
-                if self.start_datetime is None:
-                    self.start_datetime = dt
-                time_diff = (dt - self.start_datetime).total_seconds()
-                if len(row) > 4:
-                    self.test_events[int(time_diff)].append(
-                        (row[1], row[2], row[3], row[4]))
-                else:
-                    self.test_events[int(time_diff)].append((row[1], row[2]))
-        rospy.loginfo("{} loaded successfully with total_events={}".format(file_name, len(self.test_events)))
-        self.test_events_len = len(self.test_events)
+        self.test_events = deque()
+        data_file = file_name
+        try:
+            rospy.loginfo("Loading data from {}".format(file_name))
+            with open(data_file) as csvfile:
+                data = csv.reader(csvfile, delimiter='\t', quotechar='|')
+                self.start_datetime = None
+                for row in data:
+                    rospy.loginfo(row[0])
+                    if len(row) == 0:
+                        break
+                    dt = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S.%f")
+                    if self.start_datetime is None:
+                        self.start_datetime = dt
+                    time_diff = (dt - self.start_datetime)
+                    time_diff = (time_diff.seconds * 1000000) + time_diff.microseconds
+                    if len(row) > 4:
+                        self.test_events.append((row[1], row[2], row[3], row[4], dt, time_diff))
+                    else:
+                        self.test_events.append((row[1], row[2], dt, time_diff))
+            rospy.loginfo("{} loaded successfully with total_events={}".format(file_name, len(self.test_events)))
+            return True
+        except IOError as e:
+            rospy.logerr("I/O error({0}): {1}".format(e.errno, e.strerror))
+        return False
 
     def test_task_controller(self, request):
         response = TaskStatus(
@@ -127,14 +135,16 @@ class TestErrorDetector:
             if self.task_number is None \
                and request.request.status == TaskStatus.START \
                and self.task_status.status == TaskStatus.PENDING:
-                self.load_test_events(request.file)
-                self.start_task(task_number=request.id.task_number)
-                self.task_number = request.id.task_number
-                self.pause = False
-                self.task_status.status = TaskStatus.ACTIVE
-                self.task_status.text = "ACTIVE"
-                rospy.loginfo("{}: START".format(Task.types[self.task_number]))
-                return TestTaskControllerResponse(response)
+                success = self.load_test_events(request.file)
+                if success and len(self.test_events) > 0:
+                    self.current_event = self.test_events.popleft()
+                    self.start_task(task_number=request.id.task_number)
+                    self.task_number = request.id.task_number
+                    self.pause = False
+                    self.task_status.status = TaskStatus.ACTIVE
+                    self.task_status.text = "ACTIVE"
+                    rospy.loginfo("{}: START".format(Task.types[self.task_number]))
+                    return TestTaskControllerResponse(response)
             elif self.task_number == request.id.task_number \
                and request.request.status == TaskStatus.END \
                and self.task_status.status == TaskStatus.ACTIVE:
@@ -157,48 +167,47 @@ class TestErrorDetector:
         return PauseResponse(True)
 
     def casas_publish_event(self):
-        if self.pause:
-            self.rcon.call_later(1, self.casas_publish_event)
-            return
-        if self.task_status.status == TaskStatus.ACTIVE:
-            if self.test_events_ctr == self.test_events_len:
-                self.end_task(task_number=self.task_number)
-                rospy.loginfo("No more events in file")
-                rospy.loginfo("{}: END".format(Task.types[self.task_number]))
-                self.task_number = None
-                self.pause = False
-                self.task_status.status = TaskStatus.PENDING
-                self.task_status.text = "PENDING"
-                self.rcon.call_later(1, self.casas_publish_event)
-                return
+        while True:
+            while self.task_status.status == TaskStatus.ACTIVE:
+                while self.pause:
+                    time.sleep(.5)
 
-            if self.test_event_index in self.test_events:
-                self.test_events_ctr += 1
-                rospy.loginfo("ctr={} total={}".format(self.test_events_ctr, self.test_events_len))
-
-            epoch = time.mktime(datetime.datetime.now().timetuple()) \
-                + datetime.datetime.now().microsecond * 1e-6
-
-            for event in self.test_events[self.test_event_index]:
+                epoch = time.mktime(self.current_event[-2].timetuple()) \
+                            + self.current_event[-2].microsecond * 1e-6
                 new_event = objects.Event(
                     category='test',
                     package_type='test',
                     sensor_type='test',
-                    message=event[1],
-                    target=event[0],
-                    serial='test',
-                    by='test',
-                    channel='test',
+                    message=self.current_event[1],
+                    target=self.current_event[0],
+                    serial=self.mac_address,
+                    by='ROS_Node_'+rospy.get_name()[1:],
+                    channel='rawevents',
                     site='test',
                     epoch='{:f}'.format(epoch),
-                    uuid=str(uuid.uuid4()).replace('-', ''))
+                    uuid=uuid.uuid4().hex)
                 #rospy.loginfo(new_event.get_json())
                 self.rcon.publish_to_exchange(
                     exchange_name='test.events.testbed.casas',
                     casas_object=new_event)
 
-            self.test_event_index += 1
-        self.rcon.call_later(1, self.casas_publish_event)
+                if len(self.test_events) == 0:
+                    self.end_task(task_number=self.task_number)
+                    rospy.loginfo("No more events in file")
+                    rospy.loginfo("{}: END".format(Task.types[self.task_number]))
+                    self.task_number = None
+                    self.pause = False
+                    self.task_status.status = TaskStatus.PENDING
+                    self.task_status.text = "PENDING"
+                    break
+
+                next_event = self.test_events.popleft()
+                pause_time = (next_event[-1] - self.current_event[-1]) * 1e-06
+                rospy.loginfo("num event left={} pause time={}".format(len(self.test_events), pause_time))
+                self.current_event = next_event
+                time.sleep(pause_time * self.speed)
+
+            time.sleep(.5)
 
     def casas_run(self):
         try:
@@ -216,6 +225,9 @@ class TestErrorDetector:
 
 if __name__ == "__main__":
     rospy.init_node("test_error_detector", disable_signals=True)
-    ted = TestErrorDetector()
+    if len(sys.argv) < 2:
+        ted = TestErrorDetector(speed=1)
+    else:
+        ted = TestErrorDetector(speed=float(sys.argv[1]))
     ted.casas_run()
     rospy.on_shutdown(ted.stop)
